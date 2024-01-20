4cc0b44d20927641432d8743ee931ab08b1ae4b2
solufa <solufa@users.noreply.github.com>
2024-01-20 16:46:03 +0000

    deploy: 60784bb7c55fbcdd99d9843ec39014ed80ad82fe

---
+++ _next/static/GgRLBMWaLsEYX2k_w0ugQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b059a5c4697449e7.css","static/chunks/pages/index-f0bf4f88e7458177.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
