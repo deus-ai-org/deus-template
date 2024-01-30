fd028cfebdf32eb830b0cdb9bee8278f90dd1478
solufa <solufa2020@gmail.com>
2024-01-31 05:38:55 +0900

    chore: update api test

--- server/tests/api/tasks.test.ts
+++ server/tests/api/tasks.test.ts
@@ -3,24 +3,24 @@ import { apiClient, sampleBlob } from './apiClient';
 
 test('GET: /api/public/tasks', async () => {
   const res = await apiClient.public.tasks.get();
-  expect(res.status).toEqual(200);
+  expect(res.status === 200).toBeTruthy();
 });
 
 test('GET: /api/public/tasks/_taskId@string', async () => {
   const task = await apiClient.private.tasks.post({ body: { label: 'task 2' } });
   const res = await apiClient.public.tasks._taskId(task.body.id).get();
-  expect(res.status).toEqual(200);
+  expect(res.status === 200).toBeTruthy();
 });
 
 test('POST: /api/private/tasks', async () => {
   const res = await apiClient.private.tasks.post({ body: { label: 'task 1', image: sampleBlob } });
-  expect(res.status).toEqual(201);
+  expect(res.status === 201).toBeTruthy();
 });
 
 test('GET: /api/private/tasks/_taskId@string', async () => {
   const task = await apiClient.private.tasks.post({ body: { label: 'task 2' } });
   const res = await apiClient.private.tasks._taskId(task.body.id).get();
-  expect(res.status).toEqual(200);
+  expect(res.status === 200).toBeTruthy();
 });
 
 test('PATCH: /api/private/tasks/_taskId@string', async () => {
@@ -28,11 +28,11 @@ test('PATCH: /api/private/tasks/_taskId@string', async () => {
   const res = await apiClient.private.tasks._taskId(task.body.id).patch({
     body: { label: task.body.label, done: true },
   });
-  expect(res.status).toEqual(200);
+  expect(res.status === 200).toBeTruthy();
 });
 
 test('DELETE: /api/private/tasks/_taskId@string', async () => {
   const task = await apiClient.private.tasks.post({ body: { label: 'task 3', image: sampleBlob } });
   const res = await apiClient.private.tasks._taskId(task.body.id).delete();
-  expect(res.status).toEqual(204);
+  expect(res.status === 204).toBeTruthy();
 });
